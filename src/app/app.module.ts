161e17ea5c5d6fd3ba5df1a4c4c796d243a17a36
Freek <freek_mencke@outlook.com>
2023-04-10 22:09:51 +0200

    fix(pwa): fix pwa not passing lighthouse checks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
-  imports: [BrowserModule, CoreModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: !isDevMode(),
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerImmediately',
+    }),
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
