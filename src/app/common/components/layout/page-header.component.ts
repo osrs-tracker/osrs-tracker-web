4d6c7a44301b9d872a55ae9752d2681eae01867a
Freek Mencke <freek_mencke@outlook.com>
2025-04-05 15:07:33 +0200

    fix(alignment): fix alignment of tracker headers

--- src/app/common/components/layout/page-header.component.ts
+++ src/app/common/components/layout/page-header.component.ts
@@ -4,7 +4,7 @@ import { Component, InputSignal, input } from '@angular/core';
   standalone: true,
   selector: 'page-header',
   template: `
-    <header class="container mx-auto pt-20 pb-16 md:pt-24 md:pb-20 text-center">
+    <header class="container mx-auto pt-20 px-4 pb-16 md:pt-24 md:pb-20 text-center">
       <h1 class="text-4xl sm:text-5xl lg:text-6xl font-bold text-slate-900 dark:text-white">{{ headerText() }}</h1>
       <div class="mt-12 flex flex-col items-center relative">
         <ng-content></ng-content>
